0babec9aac17c9ad15f9f24a55657c876e8c00f5
ParamaaS <paramaa138@gmail.com>
2024-02-13 11:04:57 +0700

    Restart it agaian

--- src/app/register/traveler/page.tsx
+++ src/app/register/traveler/page.tsx
@@ -3,7 +3,7 @@ import TravelerRegisterForm from "@/components/register/TravelerRegisterForm";
 export default async function TravelerRegisterPage() {
   return (
     <div className="my-12 flex min-h-screen justify-center">
-      <TravelerRegisterForm />
+      <TravelerRegisterForm2 />
     </div>
   );
 }
