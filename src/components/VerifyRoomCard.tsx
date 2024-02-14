f374e492ef34bdaa849478a2489809d0bda23faf
blackfenzer <6431346921@student.chula.ac.th>
2024-02-14 13:32:59 +0700

    find bug

--- src/components/VerifyRoomCard.tsx
+++ src/components/VerifyRoomCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Image from "next/image";
 const VerifyRoomCard = () => {
   return (
     <div className="px-4 pb-4">
@@ -8,19 +8,19 @@ const VerifyRoomCard = () => {
       </div>
       <div className="flex">
         <div className="relative">
-          <img src="/room1.jpeg" alt="Room 1" className="mr-2 h-60 w-80" />
+          <Image src="/room1.jpeg" alt="Room 1" className="mr-2 h-60 w-80" />
           <span className="absolute right-2 top-0 bg-green-500 p-1 text-xs text-white">
             Verify
           </span>
         </div>
         <div className="relative">
-          <img src="/room2.jpeg" alt="Room 2" className="mr-2 h-60 w-80" />
+          <Image src="/room2.jpeg" alt="Room 2" className="mr-2 h-60 w-80" />
           <span className="absolute right-2 top-0 bg-red-500 p-1 text-xs text-white">
             Not Verify
           </span>
         </div>
         <div className="relative">
-          <img src="/room3.jpeg" alt="Room 3" className="mr-2 h-60 w-80 " />
+          <Image src="/room3.jpeg" alt="Room 3" className="mr-2 h-60 w-80 " />
           <span className="absolute right-2 top-0 bg-green-500 p-1 text-xs text-white">
             Verify
           </span>
