beabc0c6b70c024272c21e4a72a4643fa875026c
Jodsint <jodsint@gmail.com>
2024-02-14 12:12:10 +0700

    Add Host Image and Add Admin Verification

--- src/app/hosts/page.tsx
+++ src/app/hosts/page.tsx
@@ -1,13 +1,13 @@
 // pages/extra/extra.tsx
-import phuket from "/public/phuket.png";
-import ladprao from "/public/ladprao.png";
-import pattaya from "/public/pattaya.png";
+
+import chefSmart from "/public/chefSmart.png";
+import ta from "/public/taengooTVselfie.jpg";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { Button } from "@/components/ui/button";
-
 import React from "react";
 import Properties from "@/components/hosts/properties";
+
 const Extra = async () => {
   const session = await getServerSession(authOptions);
 
@@ -19,12 +19,25 @@ const Extra = async () => {
           {" "}
           Need to Verify{" "}
         </Button>
-        <Properties imageUrl={ladprao} />{" "}
-        {/* Call the component and pass the imageUrl prop */}
-        <Properties imageUrl={ladprao} />{" "}
-        {/* Call the component and pass the imageUrl prop */}
+        <Properties imageUrl={chefSmart} /> <Properties imageUrl={ta} />{" "}
       </div>
     </div>
   );
 };
 export default Extra;
+
+//add this to parama's part "Admin (Host Profile)"
+// import pattaya from "/public/pattaya.png";
+// import phuket from "/public/phuket.png";
+// import ladprao from "/public/ladprao.png";
+// import Properties2 from "@/components/hosts/properties2";
+// return (
+//   <div>
+//     <div className="h-16 w-full bg-[#004B64]"></div>
+//     <div className="mx-auto flex max-w-prose flex-col justify-center">
+//       <Properties2 imageUrl={ladprao} />{" "}
+//       <Properties2 imageUrl={pattaya} />{" "}
+
+//     </div>
+//   </div>
+// );
