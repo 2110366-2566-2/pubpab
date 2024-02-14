ee75fcf9630e028a1802fe3b14e58e82f43f5eda
Itar <natdanaitrintawat@gmail.com>
2024-02-15 00:28:32 +0700

    reload  button

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -4,10 +4,21 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { Button, buttonVariants } from "../components/ui/button";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const ProfileButton = () => {
   const { data: session } = useSession();
+  const router = useRouter();
 
+  const handleTravelerClick = async () => {
+    await router.push("/edit/traveler");
+    setTimeout(() => location.reload(), 500);
+  };
+
+  const handleHostClick = async () => {
+    await router.push("/edit/host");
+    setTimeout(() => location.reload(), 500);
+  };
   if (session && session.user) {
     if (session.user.role == "Hosts") {
       return (
@@ -15,14 +26,15 @@ const ProfileButton = () => {
           <p className="m-2 text-center text-lg font-bold">
             {session.user.name}
           </p>
-          <Link href="/edit/host">
-            <Button
-              variant={"ghost"}
-              className="text-black hover:text-blue-600"
-            >
-              Profile
-            </Button>
-          </Link>
+          {/* <Link href="edit/host"> */}
+          <Button
+            variant={"ghost"}
+            className="text-black hover:text-blue-600"
+            onClick={handleHostClick}
+          >
+            Profile
+          </Button>
+          {/* </Link> */}
         </div>
       );
     } else if (session.user.role == "Travelers") {
@@ -31,14 +43,15 @@ const ProfileButton = () => {
           <p className="m-2 text-center text-lg font-bold">
             {session.user.name}
           </p>
-          <Link href="/edit/traveler">
-            <Button
-              variant={"ghost"}
-              className="text-black hover:text-blue-600"
-            >
-              Profile
-            </Button>
-          </Link>
+          {/* <Link href="edit/traveler"> */}
+          <Button
+            variant={"ghost"}
+            className="text-black hover:text-blue-600"
+            onClick={handleTravelerClick}
+          >
+            Profile
+          </Button>
+          {/* </Link> */}
         </div>
       );
     }
