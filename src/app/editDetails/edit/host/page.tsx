937ec74f1421124fb48c568827f39ad216046899
Thanavut <Thanavut32769@gmail.com>
2024-02-16 10:32:43 +0700

    update comment

--- src/app/editDetails/edit/host/page.tsx
+++ src/app/editDetails/edit/host/page.tsx
@@ -1,17 +1,18 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import HostEditForm from "@/components/editDetails/edit/HostEditForm";
 import { getServerSession } from "next-auth";
 
+import HostEditForm from "@/components/editDetails/edit/HostEditForm";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
 export default async function HostEditPage() {
-  // const session = await getServerSession(authOptions);
-  // if (!session?.user) {
-  //   return <h1 className="text-lg">Not logged in</h1>;
-  // }
-  // if (session.user.role !== "Hosts") {
-  //   return <h1 className="text-lg">Not a host</h1>;
-  // }
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    return <h1 className="text-lg">Not logged in</h1>;
+  }
+  if (session.user.role !== "Hosts") {
+    return <h1 className="text-lg">Not a host</h1>;
+  }
   return (
-    <div className="my-12 flex justify-center">
+    <div className="mx-auto my-12 flex max-w-3xl justify-center">
       <HostEditForm />
     </div>
   );
