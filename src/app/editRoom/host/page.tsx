9122adc6fc50bc7f8aaa9b9207e13404132dbb06
Thanavut <Thanavut32769@gmail.com>
2024-02-16 13:21:31 +0700

    delete duplicate folders

--- src/app/editRoom/host/page.tsx
+++
@@ -1,9 +0,0 @@
-import HostEditRoomForm from "@/components/editRoom/HostEditRoomForm";
-
-export default async function HostEditRoom() {
-  return (
-    <div className="my-12 flex min-h-screen justify-center">
-      <HostEditRoomForm />
-    </div>
-  );
-}
