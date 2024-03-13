fa4ede482be58b5e0e602a82b6897cf14a3caeb7
NinePoonyawat <ninekrub139@gmail.com>
2024-03-14 00:24:31 +0700

    bye
    I'm gonna give up and let you down. I will run around and desert you. I will gonna make you cry, say "GOOD BYE". tell a lie and hurt you.

--- src/components/reserve/ReserveRoom.tsx
+++ src/components/reserve/ReserveRoom.tsx
@@ -3,8 +3,11 @@
 import ReserveBookingCard from "@/components/reserve/reserve";
 import { trpc } from "@/lib/trpc/client";
 import LoadingScreen from "@/components/ui/loading-screen";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
 
 export default function ReserveRoom() {
+  const router = useRouter();
   const queryParameters = new URLSearchParams(window.location.search);
   const accom_id = queryParameters.get("accom_id");
   const room_id = queryParameters.get("room_id");
@@ -35,6 +38,10 @@ export default function ReserveRoom() {
     );
   }
 
+  const handleBackClick = () => {
+    router.back();
+  };
+
   const roomData = findRoom.data;
   const accomData = findAccom.data;
 
@@ -51,12 +58,13 @@ export default function ReserveRoom() {
   return (
     <div className="flex w-full flex-col items-center">
       <div className="p-4">
-        <button
+        <Button
+          onClick={handleBackClick}
           type="button"
-          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          className="text-grey-800 mt-15 mb-4 w-40 border border-black bg-[#F4EDEA] hover:text-white"
         >
           Back
-        </button>
+        </Button>
         <div className="flex w-full flex-col gap-4">
           <ReserveBookingCard
             accomName={accomData.name_a}
