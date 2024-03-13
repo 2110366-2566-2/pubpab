8cf85b2c7f4b8d0cb0f34deb0e53d5d83806f3ef
ParamaaS <paramaa138@gmail.com>
2024-03-13 12:12:35 +0700

    Reservation page ver1

---
+++ src/app/reserve/page.tsx
@@ -0,0 +1,30 @@
+import ReserveBookingCard from "@/components/reserve/reserve";
+
+export default function Host_Booking() {
+  return (
+    <div className="flex w-full flex-col items-center">
+      <div className="p-4">
+        <button
+          type="button"
+          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          Back
+        </button>
+        {/* <h1 className="mb-4 w-full text-3xl font-semibold">Reserved Room</h1> */}
+        <div className="flex w-full flex-col gap-4">
+          <ReserveBookingCard
+            accomName="The Base"
+            roomName="Room 1"
+            price="100"
+            location="Bangkok"
+            // adult="2"
+            // child="1"
+            // user="John Doe"
+            checkInDate="2024/02/17"
+            checkOutDate="2024/02/19"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/reserve/reserve.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+
+import EastHotelImage from "@/../../public/easthotel.jpeg";
+
+const ReserveBookingCard = ({
+  accomName,
+  roomName,
+  price,
+  //   adult,
+  //   child,
+  location,
+  checkInDate,
+  checkOutDate,
+}: {
+  accomName: string;
+  roomName: string;
+  price: string;
+  //   adult: string;
+  //   child: string;
+  location: string;
+  checkInDate: string;
+  checkOutDate: string;
+}) => {
+  return (
+    <div className="relative flex flex-row gap-2 rounded-lg bg-white shadow-md">
+      <div className="flex w-full flex-col p-4">
+        <div className="flex flex-row justify-between">
+          <span>
+            <h2 className="mb-2  text-xl">{accomName}</h2>
+            <h1 className="mb-2 text-2xl font-semibold">{roomName}</h1>
+            <h4 className="mb-2  text-xl">{location}</h4>
+            <Image
+              src={EastHotelImage}
+              alt="hotel"
+              className="max-w-md rounded-lg object-scale-down"
+            />
+          </span>
+        </div>
+      </div>
+      <div className="flex w-full flex-col p-4">
+        <div className="flex flex-row justify-between">
+          <span>
+            <h2 className="mb-2  text-xl">Total</h2>
+            {/* <h1 className="mb-2 text-2xl font-semibold">฿{price}</h1> */}
+            <p className="pb-4 text-2xl font-bold">฿{price} </p>
+          </span>
+          <span className="flex flex-col text-right">
+            {/* <p className="pb-4 text-2xl font-bold">฿{price} </p> */}
+            {/* <p>{adult} Adults</p>
+            <p>{child} Children</p> */}
+          </span>
+        </div>
+        <div className="flex h-full flex-row justify-between pt-4">
+          <span className="flex w-3/4 flex-row gap-16">
+            <div className="flex flex-row gap-5  ">
+              <h1 className="pb-1 font-semibold">{checkInDate}</h1>
+              <h1 className="pb-1 font-semibold">{checkOutDate}</h1>
+            </div>
+          </span>
+          <span className="flex items-end justify-end p-4 text-center">
+            <button className="h-15 rounded-lg bg-blue-500 px-10 text-white hover:bg-blue-600">
+              Continue payemnt
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ReserveBookingCard;
