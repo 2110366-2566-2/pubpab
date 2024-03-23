960df96df7472b1d1683194a5543b8506bd1a956
NinePoonyawat <6431329221@student.chula.ac.th>
2024-03-23 19:34:34 +0700

    chat page initiate

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,24 @@
+import MessageArea from "@/components/chat/MessageArea";
+import UserList from "@/components/chat/UserList";
+import { useState } from "react";
+
+export default async function ChatPage() {
+  const [chatId, setChatId] = useState<string>("");
+
+  // Function to update chat_id
+  const handleChatIdChange = (newChatId: string) => {
+    setChatId(newChatId);
+  };
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex" style={{ width: "100%" }}>
+        <div style={{ flex: "70%", paddingRight: "1rem" }}>
+          <UserList onChatIdChange={handleChatIdChange} />
+        </div>
+        <div style={{ flex: "30%", paddingLeft: "1rem" }}>
+          <MessageArea chat_id={chatId} />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/chat/UserList.tsx
@@ -0,0 +1,28 @@
+import { trpc } from "@/lib/trpc/client";
+import { useSession } from "next-auth/react";
+
+type OnChatIdChange = (chatId: string) => void;
+
+interface UserListProps {
+  onChatIdChange: OnChatIdChange;
+}
+
+const UserList = ({ onChatIdChange }: UserListProps) => {
+  const { data: session } = useSession();
+  const isHost = session?.user?.role === "Hosts" ? true : false;
+  const getAllChatroom = trpc.chat.chat.get.useQuery({
+    host_id: isHost ? session?.user?.id : undefined,
+    traveler_id: isHost ? undefined : session?.user?.id,
+  });
+  const getChatRoom = trpc.chat.chat.find.useQuery({
+    host_id: isHost ? session?.user?.id : "",
+    traveler_id: isHost ? "" : session?.user?.id,
+  });
+
+  const handleUserClick = (chatId: string) => {
+    onChatIdChange(chatId);
+  };
+  return <div></div>;
+};
+
+export default UserList;
