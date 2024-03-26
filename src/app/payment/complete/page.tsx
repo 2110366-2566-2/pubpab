b81e0b5162c477b216fae58c21775c55841832b1
Itar <natdanaitrintawat@gmail.com>
2024-03-26 15:29:35 +0700

    change page

--- src/app/payment/complete/page.tsx
+++ src/app/payment/complete/page.tsx
@@ -1,3 +1,25 @@
-export default function PaymentCompletePage() {
-  return <div>Payment Complete</div>;
+import Image from "next/image";
+
+export default function CompletePayment() {
+  return (
+    <div className="flex w-full flex-col items-center">
+      <div className="p-4">
+        <h1 className="mb-4 w-full text-3xl font-semibold text-[#49B019]">
+          Payment completed
+        </h1>
+        {/* <div className="flex w-full flex-col gap-4">
+            
+            </div> */}
+        <div>
+          {" "}
+          <Image
+            src="/green-check.svg"
+            width={300}
+            height={300}
+            alt="payment completed"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
