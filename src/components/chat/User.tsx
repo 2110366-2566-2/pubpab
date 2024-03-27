1f316f15b970567079162b0e2bba760a78d90b98
ParamaaS <paramaa138@gmail.com>
2024-03-27 12:22:49 +0700

    New component structured [User]

---
+++ src/components/chat/User.tsx
@@ -0,0 +1,28 @@
+import user1 from "@/../public/user1.jpg";
+import Image from "next/image";
+
+const User = () => {
+  return (
+    <li className="flex justify-between gap-x-6 py-5">
+      <div className="flex min-w-0 gap-x-4">
+        <Image
+          className="h-12 w-12 flex-none rounded-full bg-gray-50"
+          src={user1}
+          alt=""
+        />
+        <div className="min-w-0 flex-auto">
+          <p className="text-sm font-semibold leading-6 text-gray-900">
+            Son Heung-min
+          </p>
+          <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+            Last Message
+          </p>
+        </div>
+      </div>
+      <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+        <p className="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
+      </div>
+    </li>
+  );
+};
+export default User;
--- src/components/chat/UserList.tsx
+++ src/components/chat/UserList.tsx
@@ -1,6 +1,8 @@
 import { trpc } from "@/lib/trpc/client";
 import { useSession } from "next-auth/react";
 import user1 from "@/../public/user1.jpg";
+import Image from "next/image";
+import User from "@/components/chat/User";
 
 type OnChatIdChange = (chatId: string) => void;
 
@@ -28,56 +30,10 @@ const UserList = ({ onChatIdChange }: UserListProps) => {
       <div className="chats">
         <div className="userChat">
           <ul role="list" className="divide-y divide-gray-100">
-            <li className="flex justify-between gap-x-6 py-5">
-              <div className="flex min-w-0 gap-x-4">
-                <Image
-                  className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                  src={user1}
-                  alt=""
-                ></Image>
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
-                    Leslie Alexander
-                  </p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                    leslie.alexander@example.com
-                  </p>
-                </div>
-              </div>
-              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p className="text-sm leading-6 text-gray-900">
-                  Co-Founder / CEO
-                </p>
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
-                </p>
-              </div>
-            </li>
-            <li className="flex justify-between gap-x-6 py-5">
-              <div className="flex min-w-0 gap-x-4">
-                <Image
-                  className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                  src={user1}
-                  alt=""
-                ></Image>
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
-                    Michael Foster
-                  </p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                    michael.foster@example.com
-                  </p>
-                </div>
-              </div>
-              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p className="text-sm leading-6 text-gray-900">
-                  Co-Founder / CTO
-                </p>
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
-                </p>
-              </div>
-            </li>
+            <User />
+            <User />
+            <User />
+            <User />
           </ul>
         </div>
       </div>
